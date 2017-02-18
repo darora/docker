362fbef4957922c2596d74c0d73e1f1e7b23ddbc
Divyanshu Arora <darora@users.noreply.github.com>
2017-02-18 18:43:53 -0500

    Update names.go
    Make it easier to copy the container ID by adding a word break.

--- daemon/names.go
+++ daemon/names.go
@@ -73,7 +73,7 @@ func (daemon *Daemon) reserveName(id, name string) (string, error) {
 				logrus.Errorf("got unexpected error while looking up reserved name: %v", err)
 				return "", err
 			}
-			return "", fmt.Errorf("Conflict. The container name %q is already in use by container %q. You have to remove (or rename) that container to be able to reuse that name.", name, id)
+			return "", fmt.Errorf("Conflict. The container name %q is already in use by container %q ; you have to remove (or rename) that container to be able to reuse that name.", name, id)
 		}
 		return "", fmt.Errorf("error reserving name: %q, error: %v", name, err)
 	}
